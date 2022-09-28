398dd148890aca41addbe71a1d1e2409438126fe
JonasRocha189 <jonas.rocha189@gmail.com>
2022-09-28 15:33:26 -0300

    finalizado autenticacao de usuario

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import "./Navbar.css";
 
 // Components
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, Navigate } from "react-router-dom";
 import {
   BsSearch,
   BsHouseDoorFill,
@@ -9,7 +9,30 @@ import {
   BsFillCameraFill,
 } from "react-icons/bs";
 
+// Hooks
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+// Redux
+import { logout, reset } from "../slices/authSlice";
+
 const Navbar = () => {
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
+
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+
+    navigate("/login");
+  };
+
   return (
     <nav id="nav">
       <Link to="/">ReactGram</Link>
@@ -20,22 +43,43 @@ const Navbar = () => {
       </form>
 
       <ul id="nav-links">
-        <li>
-          <NavLink to="/">
-            <BsHouseDoorFill />
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink to="/login">
-            <span>Entrar</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">
-            <span>Cadastrar</span>
-          </NavLink>
-        </li>
+        {auth ? (
+          <>
+            <li>
+              <NavLink to="/">
+                <BsHouseDoorFill />
+              </NavLink>
+            </li>
+            {user && (
+              <li>
+                <NavLink to={`/users/${user._id}`}>
+                  <BsFillCameraFill />
+                </NavLink>
+              </li>
+            )}
+            <li>
+              <NavLink to="/profile">
+                <BsFillPersonFill />
+              </NavLink>
+            </li>
+            <li>
+              <span onClick={handleLogout}>Sair</span>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to="/login">
+                <span>Entrar</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/register">
+                <span>Cadastrar</span>
+              </NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
